743aa2f8998d9edaee7426c2c82b95c912071e96
ASJ <aserrajaumot@gmail.com>
2016-11-29 15:41:48 +0100

    Model and Routes boilerplate to connect the API with MongoDB using Mongoose

--- server/api/api.js
+++ server/api/api.js
@@ -3,5 +3,6 @@ var router = require('express').Router();
 // api router will mount other routers
 // for all our resources
 router.use('/recipes', require('./recipe/recipeRoutes'));
+router.use('/users', require('./user/userRoutes'));
 
 module.exports = router;
